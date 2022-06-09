67beaf6b94bd06450491d366b7c825dcb8fc8591
radioromina <romydai26592@gmail.com>
2022-06-09 14:41:08 -0300

    Primer entrega del Proyecto final

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 alert("Vamos a comprar una tv juntos.")
 
+
 console.log(tv1.stock)
 tv1.vender = confirm(`Desea comprar la tv ${tv1.marca} ${tv1.modelo} por ${tv1.precioFinal()}?`)
 tv1.unidades = parseInt(prompt("cuantas unidades?"))
--- js/objetos.js
+++ js/objetos.js
@@ -10,12 +10,25 @@ class Producto {
         this.unidades = 0
     }
         precioFinal(){
-            return this.precio * this.iva
+            return parsefloat ((this.precio * this.iva).toFixed(2))
         }
         descuentoStock(unidades) {
             this.stock = this.stock - this.unidades 
         }
         
+        
     }
 
+const tv = []
+
+function generadorAutomatico () {
+tv.push (new Producto("samsung", 15, "89000", "UN50", 50, IVA))
+tv.push (new Producto("samsung", 12, "45000", "UN32", 32, IVA))
+tv.push (new Producto("philips", 8, "75000", "50PU", 50, IVA))
+tv.push (new Producto("philips", 13, "55000", "43PF", 43, IVA))
+tv.push (new Producto("LG", 10, "60000", "43LM", 43, IVA))
+tv.push (new Producto("LG", 15, "120000", "50UN", 50, IVA))
+tv.push (new Producto("noblex", 20, "71999", "DM50", 50, IVA))
+tv.push (new Producto("noblex", 17, "44999", "DK43", 43, IVA))
+}
 
