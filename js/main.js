875513739303fbe5f2b557b646bce160e5752393
radioromina <romydai26592@gmail.com>
2022-06-10 11:16:54 -0300

    Primer entrega del Proyecto final v2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-
 alert("Vamos a comprar una tv juntos.")
+generadorAutomatico()
+debugger
 
-
-console.log(tv1.stock)
-tv1.vender = confirm(`Desea comprar la tv ${tv1.marca} ${tv1.modelo} por ${tv1.precioFinal()}?`)
-tv1.unidades = parseInt(prompt("cuantas unidades?"))
-tv1.descuentoStock(tv1.unidades)
-console.log(tv1.stock)
+console.log(tv[0].stock)
+tv[0].vender = confirm(`Desea comprar la tv ${tv[0].marca} ${tv[0].modelo} por ${tv[0].precioFinal()}?`)
+tv[0].unidades = parseInt(prompt("cuantas unidades?"))
+tv[0].descuentoStock(tv[0].unidades)
+console.log(tv[0].stock)
--- js/objetos.js
+++ js/objetos.js
@@ -10,7 +10,7 @@ class Producto {
         this.unidades = 0
     }
         precioFinal(){
-            return parsefloat ((this.precio * this.iva).toFixed(2))
+            return parseFloat((this.precio * this.iva).toFixed(2))
         }
         descuentoStock(unidades) {
             this.stock = this.stock - this.unidades 
@@ -19,16 +19,4 @@ class Producto {
         
     }
 
-const tv = []
-
-function generadorAutomatico () {
-tv.push (new Producto("samsung", 15, "89000", "UN50", 50, IVA))
-tv.push (new Producto("samsung", 12, "45000", "UN32", 32, IVA))
-tv.push (new Producto("philips", 8, "75000", "50PU", 50, IVA))
-tv.push (new Producto("philips", 13, "55000", "43PF", 43, IVA))
-tv.push (new Producto("LG", 10, "60000", "43LM", 43, IVA))
-tv.push (new Producto("LG", 15, "120000", "50UN", 50, IVA))
-tv.push (new Producto("noblex", 20, "71999", "DM50", 50, IVA))
-tv.push (new Producto("noblex", 17, "44999", "DK43", 43, IVA))
-}
 
