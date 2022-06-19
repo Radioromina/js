1bb705bc03db60c564ed3306e0bfebddf9352a76
radioromina <romydai26592@gmail.com>
2022-06-18 21:06:33 -0300

    dom v4

--- js/arrayproductos.js
+++ js/arrayproductos.js
@@ -1,19 +1,21 @@
 //ARRAYS DE OBJETOS 
 class Producto {
-    constructor(img,marca, stock, precio, modelo, pulgadas, iva, importeFinal) {
+    constructor(img,marca, stock, precio, modelo, pulgadas,id, iva, importeFinal) {
+        
         this.img = img 
         this.marca = marca
         this.stock = stock
         this.precio = parseInt(precio)
         this.modelo = modelo
         this.pulgadas = pulgadas
-        this.iva = iva
+        this.iva = IVA
         this.vender = false
         this.unidades = 0
         this.importeFinal = this.precioFinal()
+        this.id = generadorID() 
     }
         precioFinal(){
-            return parseFloat((this.precio * this.iva).toFixed(2))
+            return parseFloat((this.precio * IVA).toFixed(2))
         }
         descuentoStock(unidades) {
             this.stock = this.stock - this.unidades 
@@ -39,4 +41,8 @@ productos.push (new Producto("../../public/img/LG50.jpg","Smart tv LG", 10, "600
 productos.push (new Producto("../../public/img/LG43.jpg","Smart tvLG", 15, "120000", "50UN", 50, IVA))
 productos.push (new Producto("../../public/img/noblex50.jpg","Smart tv Noblex", 20, "71999", "DM50", 50, IVA))
 productos.push (new Producto("../../public/img/noblex43.jpg","Smart tv Noblex", 17, "44999", "DK43", 43, IVA))
+}
+
+function generadorID(){
+    return parseInt(Math.random() * 10000)
 }
--- js/variables.js
+++ js/variables.js
@@ -1,7 +1,73 @@
 /*   */
-const carrito=[]
+let carrito=[]
 
-const listaDeProductos= document.getElementById ("listaDeProductos")
+
+const listaDeProductos = document.getElementById("listaDeProductos")
+
+mostrarProductos(productos)
+
+function mostrarProductos(array){
+
+array.forEach(el=>{
+    let div = document.createElement("div")
+    div.innerHTML = `
+    <div class="flex-row justify-content-center">
+        <div class="card w-50 flex-wrap" >
+          <img src="${el.img}" class="card-img-top">
+          <div class="card-body">
+            <h5> ${el.marca}, ${el.pulgadas}</h5>
+            <p>Precio Final:$${el.importeFinal}</p>
+            <button type="button" id="boton${el.id}" class=" btn btn-warning">Agregar al Carrito</button>
+          </div>
+        </div>
+    `
+    listaDeProductos.appendChild(div)
+    let btnAgregar = document.getElementById(`boton${el.id}`)
+    btnAgregar.addEventListener("click",()=>{
+        agregarAlCarrito(el.id)
+    })
+})
+}
+
+function agregarAlCarrito(id) {
+    let productoAgregar = productos.find(obj=>obj.id === id)
+    carrito.push (productoAgregar)
+    mostrarCarrito(productoAgregar)
+    actualizarCarrito()
+}
+
+function mostrarCarrito(productoAgregar){
+    let div = document.createElement("div")
+    div.innerHTML=`
+    <p>${productoAgregar.marca}, ${productoAgregar.modelo}, Precio Final: $${productoAgregar.importeFinal}</p>
+    <button type="button" id="eliminar${productoAgregar.id}" class="btn btn-warning">eliminar del carrito</button>
+    `
+    listCarrito.appendChild(div)
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
+    btnEliminar.addEventListener("click", ()=>{
+        btnEliminar.parentElement.remove()
+        carrito = carrito.filter(el=> el.id !== productoAgregar.id)
+        actualizarCarrito()
+        console.log(carrito)
+    })
+
+}
+
+function actualizarCarrito(){
+    precioTotal.innerHTML = carrito.length
+    precioTotal.innerHTML = carrito.reduce((acc,el)=> acc + el.importeFinal, 0)
+}
+
+
+
+
+
+
+
+
+
+
+/* const listaDeProductos= document.getElementById ("listaDeProductos")
 const listCarrito= document.querySelector("#listCarrito")
 
 const cargarProductos = (elemento)=> { 
@@ -28,34 +94,29 @@ const cargarProductos = (elemento)=> {
     }
 }
 
-let boton = document.getElementById("btnPrincipal")
-boton.addEventListener("click", respuestaClick)
-function respuestaClick() {
-    console.log("Respuesta evento");
-}
 
 cargarProductos()
 
-const agregarAlCarrito = (productos)=> { 
-    if (productos > "") {
-      
-        const id = productos + "enCarrito" 
-        const liCarrito = document.createElement("li")
-        liCarrito.innerText = productos
-        liCarrito.id = id
-        liCarrito.addEventListener("dblclick", ()=> {
-            eliminarDelCarrito(`${id}`)
-        })
-        listCarrito.append(liCarrito)
+const agregarAlCarrito = (modelo, img, precio)=> { 
+    if (productos != "") {
+        const item = document.createElement("li")
+        item.innerHTML = `<div>
+        <h2>${modelo}</h2>
+        <img src="${img}" alt="">
+        <p>Precio: $${precio}</p>
+    </div>`
+        item.id = productos + "enCarrito"
+        listCarrito.append(item) 
     }
-}
+} 
+
 const eliminarDelCarrito = (productoID)=> { 
     if (confirm("¿Desea eliminar el producto del carrito?")) {
         const itemAeliminar = document.getElementById(productoID)
-              itemAeliminar.remove()
-              return
+            itemAeliminar.remove()
+        return
     }
-}
+} */
 /* generadorAutomatico() */
 
 
